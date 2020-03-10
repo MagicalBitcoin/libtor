72fa826b17ab84e7528f7262df2751be2aa1d641
Alekos Filini <alekos.filini@gmail.com>
2020-03-10 23:43:40 +0100

    Big updates to the proc-macro, add many more options

--- libtor-derive/src/lib.rs
+++ libtor-derive/src/lib.rs
@@ -3,76 +3,234 @@ extern crate proc_macro2;
 extern crate quote;
 extern crate syn;
 
-use syn::{parse_macro_input, DeriveInput, Data, Fields};
-use syn::spanned::Spanned;
 use proc_macro2::{Ident, TokenStream};
 use quote::{quote, quote_spanned};
+use syn::parse::{Parse, ParseStream};
+use syn::spanned::Spanned;
+use syn::{braced, parenthesized, parse_macro_input, token, Data, DeriveInput, Fields, Token};
+
+#[derive(Debug)]
+struct ExpandToArg {
+    keyword: Ident,
+    equal_token: Token![=],
+    name: syn::Lit,
+}
+
+impl Parse for ExpandToArg {
+    fn parse(input: ParseStream) -> Result<Self, syn::Error> {
+        Ok(ExpandToArg {
+            keyword: input.parse()?,
+            equal_token: input.parse()?,
+            name: input.parse()?,
+        })
+    }
+}
+
+#[derive(Debug)]
+struct TestStruct {
+    keyword: Ident,
+    eq_token: Token![=],
+    args_group: Option<TokenStream>,
+    arrow_token: Token![=>],
+    expected: syn::LitStr,
+}
+
+impl Parse for TestStruct {
+    fn parse(input: ParseStream) -> Result<Self, syn::Error> {
+        let keyword: Ident = input.parse()?;
+        if keyword != "test" {
+            return Err(syn::Error::new(keyword.span(), "expected `test`"));
+        }
+
+        let eq_token = input.parse()?;
+
+        let args_group: Option<TokenStream> = if input.peek(token::Brace) {
+            let content;
+            braced!(content in input);
+            let content: TokenStream = content.parse()?;
+
+            Some(quote! {
+                { #content }
+            })
+        } else if input.peek(token::Paren) {
+            let content;
+            parenthesized!(content in input);
+            let content: TokenStream = content.parse()?;
+
+            Some(quote! {
+                ( #content )
+            })
+        } else {
+            None
+        };
+
+        let arrow_token = input.parse()?;
+        let expected = input.parse()?;
+        if let syn::Lit::Str(expected) = expected {
+            Ok(TestStruct {
+                keyword,
+                eq_token,
+                args_group,
+                arrow_token,
+                expected,
+            })
+        } else {
+            Err(syn::Error::new(keyword.span(), "expected a string literal"))
+        }
+    }
+}
 
 #[proc_macro_derive(Expand, attributes(expand_to))]
 pub fn derive_helper_attr(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
-    //println!("{:#?}", input);
     let enum_name = &input.ident;
 
-    let match_body = match input.data {
+    let (match_body, test_funcs) = match input.data {
         Data::Enum(data) => {
             let mut stream = TokenStream::new();
-            for variant in data.variants {
+            let mut test_stream = TokenStream::new();
+
+            'outer: for variant in data.variants {
                 let span = &variant.span();
                 let name = &variant.ident;
-                let name_string = name.to_string();
+                let mut name_string = name.to_string();
+                let mut test_count = 0;
 
-                let tokens = if variant.attrs.is_empty() {
-                    if let Fields::Named(_) = variant.fields {
-                        quote_spanned!{*span=>
-                            #enum_name::#name{..} => compile_error!("Named fields require an explicit expansion attribute"),
-                        }
-                    } else if let Fields::Unnamed(fields) = variant.fields {
-                        let fmt_str = (0..fields.unnamed.len()).into_iter().map(|_| "{}").collect::<Vec<&str>>().join(" ");
-                        let fmt_str = format!("{{}} \"{}\"", fmt_str); // {cmdName} + Wrap all the params between quotes
-                        let fmt_params = (0..fields.unnamed.len()).into_iter().map(|i| Ident::new(&format!("p_{}", i), i.span()));
-                        let expand_params = fmt_params.clone();
+                let mut fmt_attr = None;
 
-                        quote_spanned!{*span=>
-                            #enum_name::#name(#(#expand_params, )*) => format!(#fmt_str, #name_string, #(#fmt_params, )*),
+                for attr in &variant.attrs {
+                    if attr.path.get_ident().is_none()
+                        || attr.path.get_ident().unwrap() != "expand_to"
+                    {
+                        continue;
+                    }
+
+                    if let Ok(_) = attr.parse_args::<syn::Lit>() {
+                        fmt_attr = Some(attr);
+                    } else if let Ok(arg) = attr.parse_args::<ExpandToArg>() {
+                        if arg.keyword == "rename" {
+                            if let syn::Lit::Str(lit_str) = arg.name {
+                                name_string = lit_str.value();
+                            } else {
+                                let tokens = TokenStream::from(quote_spanned! {*span=>
+                                    #enum_name::#name{..} => compile_error!("`rename` must be followed by a string literal, eg #[expand_to(rename = \"example\")]"),
+                                });
+                                stream.extend(TokenStream::from(tokens));
+                            }
+                        } else if arg.keyword == "with" {
+                            let tokens = if let syn::Lit::Str(lit_str) = arg.name {
+                                let ident = Ident::new(&lit_str.value(), *span);
+
+                                let matcher = match variant.fields {
+                                    Fields::Unnamed(_) => quote! {
+                                        #enum_name::#name(..)
+                                    },
+                                    Fields::Named(_) => quote! {
+                                        #enum_name::#name{..}
+                                    },
+                                    Fields::Unit => quote! {
+                                        #enum_name::#name()
+                                    },
+                                };
+
+                                quote_spanned! {*span=>
+                                    #matcher => format!("{}", #ident(self)),
+                                }
+                            } else {
+                                quote_spanned! {*span=>
+                                    #enum_name::#name{..} => compile_error!("`with` must be followed by a string literal, eg #[expand_to(with = \"my_custom_function\")]"),
+                                }
+                            };
+
+                            stream.extend(TokenStream::from(tokens));
+                            continue 'outer;
                         }
                     } else {
-                        quote!{
-                            #enum_name::#name => #name_string.to_string(),
+                        // TODO: add those example as doc attributes
+                        if let Ok(parsed) = attr.parse_args::<TestStruct>() {
+                            let test_name =
+                                Ident::new(&format!("TEST_{}_N{}", name, test_count), *span);
+                            let args_group = &parsed.args_group.unwrap_or(TokenStream::new());
+                            let expected = &parsed.expected;
+
+                            let tokens = quote_spanned! {*span=>
+                                #[test]
+                                fn #test_name() {
+                                    use Expand;
+
+                                    let v = #enum_name::#name#args_group;
+                                    println!("{:?} => {}", v, v.expand());
+                                    assert_eq!(v.expand(), #expected);
+                                }
+                            };
+
+                            test_stream.extend(TokenStream::from(tokens));
+                            test_count += 1;
                         }
                     }
-                } else {
-                    let attr = &variant.attrs[0];
-                    let args: TokenStream = attr.parse_args().unwrap();
+                }
 
-                    if let Fields::Named(fields) = variant.fields {
-                        let ignore_filter = |field: &&syn::Field| {
-                            !field.attrs.iter().any(|a| {
-                                a.parse_args::<syn::Ident>().and_then(|ident| Ok(ident == "ignore")).unwrap_or(false)
-                            })
-                        };
+                let ignore_filter = |field: &&syn::Field| {
+                    !field.attrs.iter().any(|a| {
+                        a.parse_args::<syn::Ident>()
+                            .and_then(|ident| Ok(ident == "ignore"))
+                            .unwrap_or(false)
+                    })
+                };
+                let tokens = match (variant.fields, fmt_attr) {
+                    (Fields::Named(_), None) => {
+                        quote_spanned! {*span=>
+                            #enum_name::#name{..} => compile_error!("Named fields require an explicit expansion attribute"),
+                        }
+                    }
+                    (Fields::Named(fields), Some(attr)) => {
+                        let args: TokenStream = attr.parse_args().unwrap();
 
                         let fmt_params = fields.named.iter().filter(ignore_filter).map(|f| {
                             let ident = &f.ident;
-                            quote_spanned!{f.span()=> #ident = #ident }
+                            quote_spanned! {f.span()=> #ident = #ident }
                         });
                         let expand_params = fields.named.iter().map(|f| {
                             let ident = &f.ident;
-                            quote_spanned!{f.span()=> #ident }
+                            quote_spanned! {f.span()=> #ident }
                         });
 
-                        quote_spanned!{attr.span()=>
+                        quote_spanned! {attr.span()=>
                             #enum_name::#name{#(#expand_params,)*} => format!(#args, #(#fmt_params, )*),
                         }
-                    } else if let Fields::Unnamed(fields) = variant.fields {
-                        let fmt_params = (0..fields.unnamed.len()).into_iter().map(|i| Ident::new(&format!("p_{}", i), i.span()));
-                        let expand_params = fmt_params.clone();
+                    }
+                    (Fields::Unnamed(fields), attr) => {
+                        let expand_params = (0..fields.unnamed.len())
+                            .into_iter()
+                            .map(|i| Ident::new(&format!("p_{}", i), i.span()));
+                        let fmt_params = (0..fields.unnamed.len())
+                            .into_iter()
+                            .filter(|i| ignore_filter(&&fields.unnamed[*i]))
+                            .map(|i| Ident::new(&format!("p_{}", i), i.span()));
 
-                        quote_spanned!{*span=>
-                            #enum_name::#name(#(#expand_params, )*) => format!(#args, #(#fmt_params, )*),
+                        if attr.is_none() {
+                            let fmt_str = (0..fields.unnamed.len())
+                                .into_iter()
+                                .map(|_| "{}")
+                                .collect::<Vec<&str>>()
+                                .join(" ");
+                            let fmt_str = format!("{{}} \"{}\"", fmt_str); // {cmdName} + Wrap all the params between quotes
+                            quote_spanned! {*span=>
+                                #enum_name::#name(#(#expand_params, )*) => format!(#fmt_str, #name_string, #(#fmt_params, )*),
+                            }
+                        } else {
+                            let args: TokenStream = attr.unwrap().parse_args().unwrap();
+                            quote_spanned! {*span=>
+                                #enum_name::#name(#(#expand_params, )*) => format!(#args, #(#fmt_params, )*),
+                            }
                         }
-                    } else {
-                        quote!{
+                    }
+                    (Fields::Unit, None) => quote! {
+                        #enum_name::#name => #name_string.to_string(),
+                    },
+                    (Fields::Unit, Some(attr)) => {
+                        let args: TokenStream = attr.parse_args().unwrap();
+                        quote! {
                             #enum_name::#name => #args.to_string(),
                         }
                     }
@@ -81,11 +239,17 @@ pub fn derive_helper_attr(input: proc_macro::TokenStream) -> proc_macro::TokenSt
                 stream.extend(TokenStream::from(tokens));
             }
 
-            stream
-        },
+            (stream, test_stream)
+        }
         _ => unimplemented!(),
     };
     let match_body = TokenStream::from(match_body);
+    let test_funcs = TokenStream::from(test_funcs);
+
+    let test_mod_name = Ident::new(
+        &format!("_GENERATED_TESTS_FOR_{}", enum_name),
+        enum_name.span(),
+    );
 
     let name = input.ident;
     let expanded = quote! {
@@ -97,7 +261,12 @@ pub fn derive_helper_attr(input: proc_macro::TokenStream) -> proc_macro::TokenSt
                 }
             }
         }
+
+        #[cfg(test)]
+        mod #test_mod_name {
+            #test_funcs
+        }
     };
-    
+
     proc_macro::TokenStream::from(expanded)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -6,18 +6,407 @@ pub trait Expand: std::fmt::Debug {
     fn expand(&self) -> String;
 }
 
-#[derive(Debug, Expand)]
+#[derive(Debug, Clone, Copy)]
+pub enum SizeUnit {
+    Bytes,
+    KBytes,
+    MBytes,
+    GBytes,
+    TBytes,
+    Bits,
+    KBits,
+    MBits,
+    GBits,
+    TBits,
+}
+
+// TODO: make as a macro, DisplayLikeDebug
+macro_rules! display_like_debug {
+    ($type:ty) => {
+        impl std::fmt::Display for $type {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                write!(f, "{:?}", self)
+            }
+        }
+    };
+}
+
+display_like_debug!(SizeUnit);
+
+#[derive(Debug, Clone, Copy)]
+pub enum TorBool {
+    True,
+    False,
+    Enabled,
+    Disabled,
+}
+
+impl std::fmt::Display for TorBool {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        use TorBool::*;
+
+        let val = match self {
+            True | Enabled => 1,
+            False | Disabled => 0,
+        };
+        write!(f, "{}", val)
+    }
+}
+
+impl From<bool> for TorBool {
+    fn from(other: bool) -> TorBool {
+        if other {
+            TorBool::True
+        } else {
+            TorBool::False
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum ControlPortFlag {
+    GroupWritable,
+    WorldWritable,
+    RelaxDirModeCheck,
+}
+
+display_like_debug!(ControlPortFlag);
+display_like_debug!(SocksPortFlag);
+display_like_debug!(SocksPortIsolationFlag);
+
+#[derive(Debug, Clone, Copy)]
+pub enum LogLevel {
+    Debug,
+    Info,
+    Notice,
+    Warn,
+    Err,
+}
+
+#[derive(Debug, Clone)]
+pub enum LogDestination {
+    Stdout,
+    Stderr,
+    Syslog, // TODO: only unix
+    File(String),
+    Android, // TODO: only android
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum LogDomain {
+    General,
+    Crypto,
+    Net,
+    Config,
+    Fs,
+    Protocol,
+    Mm,
+    Http,
+    App,
+    Control,
+    Circ,
+    Rend,
+    Bug,
+    Dir,
+    Dirserv,
+    Or,
+    Edge,
+    Acct,
+    Hist,
+    Handshake,
+    Heartbeat,
+    Channel,
+    Sched,
+    Guard,
+    Consdiff,
+    Dos,
+    Process,
+    Pt,
+    Btrack,
+    Mesg,
+}
+
+fn log_expand(flag: &TorFlag) -> String {
+    let (levels, dest) = match flag {
+        TorFlag::Log(level) => (vec![(vec![], *level)], None),
+        TorFlag::LogTo(level, dest) => (vec![(vec![], *level)], Some(dest)),
+        TorFlag::LogExpanded(expanded_level, dest) => (expanded_level.clone(), Some(dest)),
+        _ => unimplemented!(),
+    };
+
+    let levels_str = levels
+        .iter()
+        .map(|(domains, level)| {
+            let mut concat_str = domains
+                .iter()
+                .map(|(enabled, domain)| {
+                    let enabled_char = if *enabled { "" } else { "~" };
+                    format!("{}{:?}", enabled_char, domain)
+                })
+                .collect::<Vec<String>>()
+                .join(",");
+            if !concat_str.is_empty() {
+                concat_str = format!("[{}]", concat_str);
+            }
+
+            format!("{}{:?}", concat_str, level).to_lowercase()
+        })
+        .collect::<Vec<String>>()
+        .join(" ");
+    let dest_str = dest
+        .and_then(|d| Some(format!(" {:?}", d).to_lowercase()))
+        .unwrap_or(String::new());
+
+    format!("Log \"{}{}\"", levels_str, dest_str)
+}
+
+#[derive(Debug, Clone)]
+pub struct DisplayVec<T: std::fmt::Debug + std::fmt::Display> {
+    vec: Vec<T>,
+}
+
+impl<T: std::fmt::Debug + std::fmt::Display> std::fmt::Display for DisplayVec<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let joined: String = self
+            .vec
+            .iter()
+            .map(|v| format!("{}", v))
+            .collect::<Vec<String>>()
+            .join(",");
+        write!(f, "{}", joined)
+    }
+}
+
+impl<T: std::fmt::Debug + std::fmt::Display> From<Vec<T>> for DisplayVec<T> {
+    fn from(vec: Vec<T>) -> DisplayVec<T> {
+        DisplayVec { vec }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct DisplayOption<T: std::fmt::Debug + std::fmt::Display> {
+    option: Option<T>,
+}
+
+impl<T: std::fmt::Debug + std::fmt::Display> std::fmt::Display for DisplayOption<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match &self.option {
+            Some(val) => write!(f, "{}", val),
+            None => Ok(()),
+        }
+    }
+}
+
+impl<T: std::fmt::Debug + std::fmt::Display> From<Option<T>> for DisplayOption<T> {
+    fn from(option: Option<T>) -> DisplayOption<T> {
+        DisplayOption { option }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum SocksPortFlag {
+    NoIPv4Traffic,
+    IPv6Traffic,
+    PreferIPv6,
+    NoDNSRequest,
+    NoOnionTraffic,
+    OnionTrafficOnly,
+    CacheIPv4DNS,
+    CacheIPv6DNS,
+    GroupWritable,
+    WorldWritable,
+    CacheDNS,
+    UseIPv4Cache,
+    UseIPv6Cache,
+    UseDNSCache,
+    PreferIPv6Automap,
+    PreferSOCKSNoAuth,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum SocksPortIsolationFlag {
+    IsolateClientAddr,
+    IsolateSOCKSAuth,
+    IsolateClientProtocol,
+    IsolateDestPort,
+    IsolateDestAddr,
+    KeepAliveIsolateSOCKSAuth,
+}
+
+trait OptionVecToString {
+    fn option_vec_to_string(&self) -> String;
+}
+
+impl<T: std::fmt::Debug> OptionVecToString for Option<Vec<T>> {
+    fn option_vec_to_string(&self) -> String {
+        self.as_ref()
+            .and_then(|flags| {
+                Some(
+                    flags
+                        .iter()
+                        .map(|f| format!("{:?} ", f))
+                        .collect::<String>(),
+                )
+            })
+            .unwrap_or("".to_string())
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum TorAddress {
+    Port(u16),
+    Address(String),
+    AddressPort(String),
+    #[cfg(target_family = "unix")]
+    Unix(String),
+}
+
+impl std::fmt::Display for TorAddress {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            TorAddress::Port(port) => write!(f, "{}", port),
+            TorAddress::Address(addr) => write!(f, "{}", addr),
+            TorAddress::AddressPort(addr) => write!(f, "{}", addr),
+            #[cfg(target_family = "unix")]
+            TorAddress::Unix(path) => write!(f, "unix:{}", path),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum HiddenServiceVersion {
+    V2 = 2,
+    V3 = 3,
+}
+
+impl std::fmt::Display for HiddenServiceVersion {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", *self as u8)
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum HiddenServiceAuthType {
+    Basic,
+    Stealth,
+}
+
+#[derive(Debug, Clone, Expand)]
 pub enum TorFlag {
     #[expand_to("-f {}")]
+    #[expand_to(test = ("filename".into()) => "-f filename")]
     ConfigFile(String),
     #[expand_to("--passphrase-fd {}")]
     PassphraseFD(u32),
+
+    BandwidthRate(usize, SizeUnit),
+    BandwidthBurst(usize, SizeUnit),
+    DisableNetwork(TorBool),
+
+    ControlPort(u16),
+    #[expand_to("ControlPort auto")]
+    ControlPortAuto,
+    #[expand_to("ControlPort \"{} {}\"")]
+    ControlPortAddress(TorAddress, DisplayOption<DisplayVec<ControlPortFlag>>),
+
+    // TODO: cfg only on unix
+    ControlSocket(String),
+    ControlSocketsGroupWritable(TorBool),
+
+    HashedControlPassword(String),
+    CookieAuthentication(TorBool),
+    CookieAuthFile(String),
+    CookieAuthFileGroupReadable(TorBool),
+    ControlPortWriteToFile(String),
+    ControlPortFileGroupReadable(TorBool),
+
+    DataDirectory(String),
+    DataDirectoryGroupReadable(TorBool),
+    CacheDirectory(String),
+    CacheDirectoryGroupReadable(String),
+
+    HTTPSProxy(String),
+    #[expand_to("HTTPSProxyAuthenticator \"{}:{}\"")]
+    HTTPSProxyAuthenticator(String, String),
+    Socks4Proxy(String),
+    Socks5Proxy(String),
+    Socks5ProxyUsername(String),
+    Socks5ProxyPassword(String),
+
+    UnixSocksGroupWritable(TorBool),
+
+    KeepalivePeriod(usize),
+
+    #[expand_to(with = "log_expand")]
+    Log(LogLevel),
+    #[expand_to(with = "log_expand")]
+    LogTo(LogLevel, LogDestination),
+    #[expand_to(with = "log_expand")]
+    LogExpanded(Vec<(Vec<(bool, LogDomain)>, LogLevel)>, LogDestination),
+    LogMessageDomains(TorBool),
+
+    LogTimeGranularity(usize),
+    TruncateLogFile(TorBool),
+    SyslogIdentityTag(String),
+    AndroidIdentityTag(String),
+    SafeLogging(TorBool), // TODO: 'relay' unsupported at the moment
+
+    PidFile(String),
+    ProtocolWarnings(TorBool),
+
+    User(String),
+    NoExec(TorBool),
+
+    Bridge(String, String, String),
+
+    ConnectionPadding(TorBool), // TODO: 'auto' not supported at the moment
+    ReducedConnectionPadding(TorBool),
+    CircuitPadding(TorBool),
+    ReducedCircuitPadding(TorBool),
+
+    ExcludeNodes(DisplayVec<String>),
+    ExcludeExitNodes(DisplayVec<String>),
+    ExitNodes(DisplayVec<String>),
+    MiddleNodes(DisplayVec<String>),
+    EntryNodes(DisplayVec<String>),
+    StrictNodes(TorBool),
+
+    FascistFirewall(TorBool),
+    FirewallPorts(DisplayVec<u16>),
+
+    MapAddress(String, String),
+    NewCircuitPeriod(usize),
+
+    SocksPort(u16),
+    #[expand_to("SocksPort auto")]
+    SocksPortAuto,
+    #[expand_to(rename = "SocksPort")]
+    SocksPortAddress(
+        TorAddress,
+        DisplayOption<DisplayVec<SocksPortFlag>>,
+        DisplayOption<DisplayVec<SocksPortIsolationFlag>>,
+    ),
+    SocksTimeout(usize),
+    SafeSocks(TorBool),
+    TestSocks(TorBool),
+
+    UpdateBridgesFromAuthority(TorBool),
+    UseBridges(TorBool),
+
+    HiddenServiceDir(String),
+    HiddenServicePort(TorAddress, DisplayOption<TorAddress>),
+    HiddenServiceVersion(HiddenServiceVersion),
+    #[expand_to("HiddenServiceAuthorizeClient {:?} {}")]
+    HiddenServiceAuthorizeClient(HiddenServiceAuthType, DisplayVec<String>),
+    HiddenServiceAllowUnknownPorts(TorBool),
+    HiddenServiceMaxStreams(usize),
+    HiddenServiceMaxStreamsCloseCircuit(TorBool),
 }
 
 #[derive(Debug, Expand)]
 pub enum TorSubcommand {
     #[expand_to("--hash-password {password}")]
-    HashPassword{password: String},
+    HashPassword { password: String },
     #[expand_to("--verify-config")]
     VerifyConfig,
     #[expand_to("--list-fingerprint")]
@@ -25,16 +414,16 @@ pub enum TorSubcommand {
     #[expand_to("--version")]
     Version,
     #[expand_to("--keygen")]
-    Keygen{
+    Keygen {
         #[expand_to(ignore)]
-        password: Option<String>
+        password: Option<String>,
     },
     #[expand_to("--keygen --newpass")]
-    KeygenNewpass{
+    KeygenNewpass {
         #[expand_to(ignore)]
         old_password: Option<String>,
         #[expand_to(ignore)]
-        new_password: Option<String>
+        new_password: Option<String>,
     },
 }
 
@@ -46,13 +435,16 @@ pub struct Tor {
 #[cfg(test)]
 mod tests {
     use crate::*;
+    use std::net::ToSocketAddrs;
 
     #[test]
     fn it_works() {
-        let config_file = TorFlag::ConfigFile("file".to_string());
-        println!("{}", config_file.expand());
-
-        let a = TorSubcommand::KeygenNewpass{old_password: None, new_password: None};
+        use std::str::FromStr;
+        let a = TorFlag::SocksPortAddress(
+            TorAddress::AddressPort("[2001:0db8:85a3:0000:0000:8a2e:0370:7334]:8080".into()),
+            Some(vec![SocksPortFlag::NoIPv4Traffic].into()).into(),
+            None.into(),
+        );
         println!("{}", a.expand());
     }
 }
